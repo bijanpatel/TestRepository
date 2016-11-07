4d06d4af10f94b14d4a7ff21196e782777a32579
bijan.patel <bijan.patel@10.118.243.66>
2016-11-07 19:22:17 +0530

    First Change : added syso

--- TestProject/src/StringTest.java
+++ TestProject/src/StringTest.java
@@ -4,6 +4,7 @@ public class StringTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name = "bij\"a\"n";
+		System.out.println("repository test");
 		System.out.println(name);
 	}
 
